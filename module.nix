1b07efcee7d4a8fa145668308a6b8f4142688558
Alexandria P. <alxpettit@gmail.com>
2024-03-26 01:06:31 -0600

    we'll see

--- module.nix
+++ module.nix
@@ -1,6 +1,18 @@
 { config, pkgs, lib, ... }: let
   cfg = config.programs.nixpak-flatpak-wrapper;
   fmt = pkgs.formats.toml {};
+  package = import ./pkg.nix { inherit pkgs lib; };
+  wrapperPackage = pkgs.symlinkJoin {
+    name = "flatpak";
+    meta.mainProgram = "flatpak";
+    paths = [ pkgs.flatpak ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postInstall = ''
+      mv "$out/bin/flatpak" "$out/bin/flatpak-raw"
+      cp "${package}/bin/flatpak" "$out/bin/flatpak"
+    '';
+  };
+
 in {
   options.programs.nixpak-flatpak-wrapper = {
     rawStructuredConfig = lib.mkOption {
@@ -15,7 +27,8 @@ in {
 
   config = lib.mkIf cfg.enable (lib.mkBefore {
     environment.systemPackages = [
-      (import ./pkg.nix { inherit pkgs lib; })
+      wrapperPackage
+
     ];
     environment.etc."nixpak-flatpak-wrapper.toml".source = fmt.generate "nixpak-flatpak-wrapper.toml" cfg.rawStructuredConfig;
   });
