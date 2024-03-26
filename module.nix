6bf19b6c12b0a2ad344a6b2fe2c1f36e2bfd602e
Alexandria P. <alxpettit@gmail.com>
2024-03-26 01:26:40 -0600

    come on barbie lets go party

--- module.nix
+++ module.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }: let
   cfg = config.programs.nixpak-flatpak-wrapper;
   fmt = pkgs.formats.toml {};
-  package = import ./pkg.nix { inherit pkgs lib; };
+  flatpak-wrapper = import ./pkg.nix { inherit pkgs lib; };
   wrapperPackage = pkgs.symlinkJoin {
     name = "flatpak";
     meta.mainProgram = "flatpak";
@@ -9,7 +9,7 @@
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postInstall = ''
       mv "$out/bin/flatpak" "$out/bin/flatpak-raw"
-      cp "${package}/bin/flatpak" "$out/bin/flatpak"
+      cp "${flatpak-wrapper}/bin/flatpak" "$out/bin/flatpak"
     '';
   };
 
@@ -23,12 +23,15 @@ in {
       type = lib.types.bool;
       default = false;
     };
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.flatpak;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [
       wrapperPackage
-
     ];
     environment.etc."nixpak-flatpak-wrapper.toml".source = fmt.generate "nixpak-flatpak-wrapper.toml" cfg.rawStructuredConfig;
   };
